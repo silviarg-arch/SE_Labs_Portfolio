4ecbb9f75858455f88b7d12a34a6f46926f8fdc7
NICOLAS PEREZ GOMEZ <nicpergom@alum.us.es>
2026-04-27 18:18:39 +0200

    Test TrafficLightController not working

--- LABS/Lab6_TDD/traffic-light-controllerTDD/src/test/java/traffic/TrafficLightControllerTest.java
+++ LABS/Lab6_TDD/traffic-light-controllerTDD/src/test/java/traffic/TrafficLightControllerTest.java
@@ -7,4 +7,9 @@ class TrafficLightControllerTest {
     @Test
     void placeholderTest() {
     }
+    @Test
+    void initial_state_should_be_red() {
+        TrafficLightController tlc = new TrafficLightController();
+        assertEquals(LightColor.RED, tlc.getCurrentColor());
+}
 }
